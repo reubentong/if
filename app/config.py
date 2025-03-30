6aa9d5f6e3e9d01abc1f4695844ff3c55c6358c5
reubentong <reuben.tong@hotmail.com>
2025-03-30 21:21:12 +0100

    Fix settings to use pydantic v2 env

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DOG_API_BASE_URL: str = "https://myapi.co.uk"
+    DOG_API_BASE_URL: str = "https://dogapi.dog/api/v2"
     DOG_API_TIMEOUT_S: int = 10
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
